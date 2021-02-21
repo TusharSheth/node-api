ff1b57177c64dba475f64520093fde1bddbca671
Tushar Sheth <tushar@zuru.tech>
2021-02-21 19:02:08 +0530

    feat: lint fix

--- src/seed.js
+++ src/seed.js
@@ -1,40 +1,38 @@
-var seeder = require('mongoose-seed');
-const { mongo, env } = require('./config/vars');
-
-// Connect to MongoDB via Mongoose
-seeder.connect(mongo.uri, function () {
-
-    // Load Mongoose models
-    seeder.loadModels([
-        './src/api/services/org/org.model.js',
-    ]);
-
-    // Clear specified collections
-    seeder.clearModels(['Org'], function () {
-        console.log('>>>', 'seed.js 14');
-
-        // Callback to populate DB once collections have been cleared
-        seeder.populateModels(data, function () {
-            seeder.disconnect();
-        });
-
-    });
-});
+const seeder = require('mongoose-seed');
+const { mongo } = require('./config/vars');
 
 // Data array containing seed data - documents organized by Model
-var data = [
-    {
-        'model': 'Org',
-        'documents': [
-            {
-                'name': 'Asian',
-            },
-            {
-                'name': 'Pacific',
-            },
-            {
-                'name': 'Eastern',
-            }
-        ]
-    }
+const data = [
+  {
+    model: 'Org',
+    documents: [
+      {
+        name: 'Asian',
+      },
+      {
+        name: 'Pacific',
+      },
+      {
+        name: 'Eastern',
+      },
+    ],
+  },
 ];
+
+// Connect to MongoDB via Mongoose
+seeder.connect(mongo.uri, () => {
+  // Load Mongoose models
+  seeder.loadModels([
+    './src/api/services/org/org.model.js',
+  ]);
+
+  // Clear specified collections
+  seeder.clearModels(['Org'], () => {
+    console.log('>>>', 'seed.js 14');
+
+    // Callback to populate DB once collections have been cleared
+    seeder.populateModels(data, () => {
+      seeder.disconnect();
+    });
+  });
+});
